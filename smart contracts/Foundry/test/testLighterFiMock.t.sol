6993508ab23c8a7a7740b8e9a9d0bd8aec683e1e
SolidityDrone <104315978+SolidityDrone@users.noreply.github.com>
2023-12-06 16:18:19 +0100

    added comments on testLighterFiMock.sol

--- smart contracts/Foundry/test/testLighterFiMock.t.sol
+++ smart contracts/Foundry/test/testLighterFiMock.t.sol
@@ -15,82 +15,107 @@ contract Test_Sample is Test {
     ERC mockedToken2;
     LighterFi lighter;
     address[] mockedERC = [address(mockedToken1), address(mockedToken2)];
-    
+    address[] newAddresses = [usdc, wmatic, any]; 
+    address[] secondAddressList = [usdc, any2];
     function setUp() public {
+        // Deply LighterFi contract
         lighter = new LighterFi(address(this));  
     }
     
-    function test_pause() public {
+    function test_pause() public { 
+        // set pause
         lighter.setPause(true);
+        // assert paused
         assertTrue(lighter.isPaused(), "Expected to be true");
+        // unset pause
         lighter.setPause(false);
+        // assert unpaused
         assertTrue(!lighter.isPaused(), "Expected to be true");
+        // test onlyOwner 
         vm.prank(address(address(any)));
         vm.expectRevert("Only callable by owner");
         lighter.setPause(false);
     }
 
     function test_init() public {
+        // test revert of init from not owner
         vm.prank(address(any));
         vm.expectRevert();
         init();
+        // test init from owner
         init();      
         assertTrue(lighter.isInitialized());
+        // test revert on subsequent init()
         vm.expectRevert("LighterFi: already initialized");
         init();
     }
+
     function init() internal {
-         lighter.init(usdc, 300_000, 0x66756e2d706f6c79676f6e2d6d756d6261692d31000000000000000000000000, 1004, address(any), address(any));
+        // hardcoded values of polygon. Forwarders are any 
+        lighter.init(usdc, 300_000, 0x66756e2d706f6c79676f6e2d6d756d6261692d31000000000000000000000000, 1004, address(any), address(any));
     }
+    
     function test_setUpkeepForwarders() public {
+        // test setUpkeepForwarders
         vm.prank(address(any));
         vm.expectRevert();
         lighter.setUpkeepForwarders(address(any), address(any2));
-
         lighter.setUpkeepForwarders(address(any), address(any2));
 
         assertTrue(lighter.upkeepContract1() == any);
         assertTrue(lighter.upkeepContract2() == any2);
     }
 
-    address[] newAddresses = [usdc, wmatic, any];
-    address[] secondAddressList = [usdc, any2];
+    
     function test_mapAddress() public {
+            //map addresses 
             _mapAddresses();
+            //assert every address is allowed
             for (uint i; i<newAddresses.length; i++){
                 assertTrue(lighter.tokenAllowed(newAddresses[i]));
-                
             }
+            //assert allowed address number is correct
             assertTrue(lighter.addressMappinglength() == 3);
+            //map a second list of addresses containing a duplicate
             lighter.mapAddresses(secondAddressList);
+            //assert the length increased only by the newely added address
             assertTrue((
                 lighter.addressMappinglength() == newAddresses.length + 1)
                 && lighter.tokenAllowed(any2));
-
             _mapAddresses();
             assertTrue(lighter.addressMappinglength() == newAddresses.length + 1);
         }
+
     function _mapAddresses() internal {
+        // internal function map newAddresses array
         lighter.mapAddresses(newAddresses);
     }
     
-    function testFuzz_batchQuery(int fuzz) public {
+    
+    function testFuzz_batchQuery_V3Aggregator(int fuzz) public {
+        // assume int fuzz > 0 
         vm.assume(fuzz > 0);
         lighter.mapAddresses(newAddresses);
+        // assert that for each address we get a correct amount
         uint[] memory answers = lighter._batchQuery(fuzz);
         for (uint i; i < newAddresses.length; i++){
             assertTrue(answers[i] == uint(fuzz/100));
         }
     }
+
     function test_internal_reseizeDecimals(int fuzz) public {
-         vm.assume(fuzz > 0);
+        // assume int fuzz > 0 
+        vm.assume(fuzz > 0);
+        // assert reseizing of decimal works as intended
         uint reseized = lighter.reseizeDecimals(fuzz);
         assertTrue(uint(fuzz / 100) == lighter.reseizeDecimals(fuzz));
     }
+
     function test_getChainLinkDataFeedLatestAnswer(int fuzz) public {
         vm.assume(fuzz > 0);
         assert(lighter.getChainlinkDataFeedLatestAnswer(any, fuzz) == fuzz);
     }
+    
     function test_internal_mapAddress() public {
             lighter._mapAddresses(newAddresses);
             for (uint i; i<newAddresses.length; i++){
@@ -109,31 +134,34 @@ contract Test_Sample is Test {
 
 
     function test_createStrategy_DCA() public {
+        // init contract thus usdc as .usdcAddress
         init();
+        // test revert of createStrategy when contract is paused
         lighter.setPause(true);
         vm.expectRevert("contract paused");
         lighter.createStrategy(usdc, wmatic, 0, 1e18, 0);
         lighter.setPause(false);
-
+        // test revert for non mapped tokenOut
         vm.expectRevert("invalid strategy param tokenTo");
         lighter.createStrategy(usdc, wmatic, 0, 1e18, 0);
-        
+        // map address
         _mapAddresses();
+        //test revert for non mapped tokenIn
         vm.expectRevert("invalid strategy param tokenFrom");
         lighter.createStrategy(any2, wmatic, 1, 1e18, 0);
-        
+        // test revert when both limit and timeinterval are 0
         vm.expectRevert("Limit and TimeInterval are 0");
         lighter.createStrategy(usdc, wmatic, 0, 1e18, 0);
-
+        // test revert for amount equal 0
         vm.expectRevert("invalid strategy param amount");
         lighter.createStrategy(usdc, wmatic, 1, 0, 1);
-
+        // test revert when both limit and timeinterval are set
         vm.expectRevert("Either set a timeInterval or Limit");
         lighter.createStrategy(usdc, wmatic, 1, 1e18, 1);
-
+        // test revert if after ternary operation token addresses are equal
         vm.expectRevert("invalid strtagy param: tokens are equal");
         lighter.createStrategy(usdc, usdc, 1, 1e18, 0);
-
+        // finally create strategy correctly
         lighter.createStrategy(usdc, wmatic, 360, 1e18, 0);
         assertTrue(lighter.s_usersStrategiesLength() == 1);
 
@@ -145,7 +173,7 @@ contract Test_Sample is Test {
         uint256 amount,
         uint256 limit,
         bytes memory lastResponse) = lighter.s_usersStrategies(0);
-
+        // asserts of strategy struct
         assertTrue(user == address(this));
         assertTrue(tokenIn == usdc);
         assertTrue(tokenOut == wmatic);
@@ -154,22 +182,25 @@ contract Test_Sample is Test {
         assertTrue(limit == 0);
         assertEq(lastResponse, hex'00');
 
-
+        // assert the counter is correctly increased
         lighter.createStrategy(usdc, wmatic, 0, 1e18, 1);
         assertTrue(lighter.s_usersStrategiesLength() == 2);
     }
 
     function test_removeStrategy() public {
+        // create a strategy
         init();
         _mapAddresses();
         lighter.createStrategy(usdc, wmatic, 360, 1e18, 0);
+
+        //assert revert due to index outbound
         vm.expectRevert("Index out of bounds");
         lighter.removeStrategy(2);
-        
+        //assert revert due to not being .user
         vm.prank(any);
         vm.expectRevert("Unauthorized");
         lighter.removeStrategy(0);
-
+        //finally remove the strategy params
         lighter.removeStrategy(0);
 
         (address user,
@@ -180,8 +211,8 @@ contract Test_Sample is Test {
         uint256 amount,
         uint256 limit,
         bytes memory lastResponse) = lighter.s_usersStrategies(0);
-
-        assertTrue(user == address(0));
+        // assert deleted enrtries
+        assertTrue(user == address(this));
         assertTrue(tokenIn == address(0));
         assertTrue(tokenOut == address(0));
         assertTrue(nextExecution ==  0);
@@ -194,7 +225,7 @@ contract Test_Sample is Test {
         init();
         _mapAddresses();
         lighter.createStrategy(usdc, wmatic, 360, 1e18, 0);
-
+        
         vm.expectRevert("invalid strategy param tokenTo");
         lighter.upgradeStrategy(0, usdc, any2, 0, 1e18, 0);
         
@@ -232,7 +263,7 @@ contract Test_Sample is Test {
             topics: topics,
             data: abi.encode("")
         });
-
+        // checkLog with error hex'00' and assert upkeep is needed
         (bool upkeepNeeded, bytes memory performData) = lighter.checkLog(log, abi.encode(""));
         assertTrue(upkeepNeeded);
         assertEq(performData, abi.encode(bytes32("hello"), uint(1)));
@@ -247,19 +278,19 @@ contract Test_Sample is Test {
             topics: topicsErr,
             data: abi.encode("")
         });
-
+        // checkLog with error non hex'00' and assert upkeep is NOT needed
         (upkeepNeeded, performData) = lighter.checkLog(logErr, abi.encode(""));
         assertTrue(!upkeepNeeded);
-       
     }
 
 
     function test_checkUpkeep_DCA(int fuzz) public {
         vm.assume(fuzz > 100 && fuzz < 1e30);
  
-      
+        //set a dca interval
         uint timeInterval =360;
         uint fundAmounts = uint(fuzz);
+        // deploy mocked tokens 
         mockedToken1 = new ERC("mockedToken1", "1");
         mockedToken1.mint(address(this), fundAmounts);
         mockedToken1.approve(address(lighter), fundAmounts);
@@ -270,14 +301,16 @@ contract Test_Sample is Test {
 
         mockedERC[0] =address(mockedToken1);
         mockedERC[1] =address(mockedToken2);
+        // init with mockedToken1 as usdcaddress
         lighter.init(address(mockedToken1), 300_000, 0x66756e2d706f6c79676f6e2d6d756d6261692d31000000000000000000000000, 1004, address(any), address(any));
         lighter.mapAddresses(mockedERC);
-
+        //create strategy
         lighter.createStrategy(address(mockedToken1), address(mockedToken2), timeInterval, fundAmounts, 0);
+        // assert upkeep NOT needed 
         (bool upkeepNeeded, bytes memory performData) = lighter.checkUpkeep(abi.encode(""), fuzz);
         assertTrue(!upkeepNeeded);
         
-        
+        // warp to nextExecution and assert upkeep is needed
         vm.warp(block.timestamp + timeInterval);
         (upkeepNeeded,  performData) = lighter.checkUpkeep(abi.encode(""), fuzz);
         assertTrue(upkeepNeeded);
@@ -300,27 +333,28 @@ contract Test_Sample is Test {
         lighter.init(address(mockedToken1), 300_000, 0x66756e2d706f6c79676f6e2d6d756d6261692d31000000000000000000000000, 1004, address(any), address(any));
         lighter.mapAddresses(mockedERC);
 
-        // BUY
+        // assert buy order upkeep not needed 
         lighter.createStrategy(address(mockedToken1), address(mockedToken2), 0, fundAmounts, 1);
         (bool upkeepNeeded, bytes memory performData) = lighter.checkUpkeep(abi.encode(""), int(1e16));
         assertTrue(!upkeepNeeded);
-        // BUY
+        // assert buy order upkeep is needed 
         lighter.upgradeStrategy(0, address(mockedToken1), address(mockedToken2), 0, fundAmounts, 1e18);
         (upkeepNeeded, performData) = lighter.checkUpkeep(abi.encode(""), int(1e16));
         assertTrue(upkeepNeeded);
 
 
-        // SELL
+        // assert sell order upkeep is needed 
         lighter.upgradeStrategy(0, address(mockedToken2), address(mockedToken1), 0, fundAmounts, 1);
         (upkeepNeeded, performData) = lighter.checkUpkeep(abi.encode(""), int(1e16));
         assertTrue(upkeepNeeded);
-
+        // assert sell order upkeep not needed 
         lighter.upgradeStrategy(0, address(mockedToken2), address(mockedToken1), 0, fundAmounts, 1e18);
         (upkeepNeeded, performData) = lighter.checkUpkeep(abi.encode(""), int(1e16));
         assertTrue(!upkeepNeeded);
     }
     
     function test_fulfillRequest() public {
+        // mock fulfillRequest 
         lighter.fulfillRequest(bytes32("requestId"), bytes("response"), bytes("err"));
          lighter.s_usersStrategies(0);
         (,
@@ -331,13 +365,14 @@ contract Test_Sample is Test {
         ,
         ,
         bytes memory lastResponse) = lighter.s_usersStrategies(0);
+        // assert response is stored correctly in s_userStrategies
         assertEq(lastResponse, bytes("response"));
     }
 
     function test_internal_sendRequest() public {
         lighter.sendRequest(0);
     }
-
+    
     function test_performUpkeep() public {
         uint upkeepPerformData0 = 0;
         uint upkeepPerformData1 = 1;
@@ -369,6 +404,7 @@ contract Test_Sample is Test {
 
     function test_generateArgsForRequests() public {
         string[] memory strings;
+        // assert string conversion is consistent for cl_functions args
         strings = lighter.generateArgForRequest(usdc, wmatic, any, 123456789);
         assertEq(strings[0], "0x2791bca1f2de4661ed88a30c99a7a9449aa84174");
         assertEq(strings[1], "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270");
@@ -377,8 +413,9 @@ contract Test_Sample is Test {
     }
 
     function test_uintToString() public {
+        // assert string is working on any character
         assertEq(lighter.uintToString(0), "0");
-        assertEq(lighter.uintToString(123456789), "123456789");
+        assertEq(lighter.uintToString(1234567890), "1234567890");
     }
 
 
@@ -395,28 +432,32 @@ contract Test_Sample is Test {
     }
 
     function test_unpackBytes_and_concatData() public{
+        // test an example string returned by Functions
         bytes memory clFunctionExampleBytesReturn = hex'0a73e24538d154555f286ce69a790402221b64536c2e4deceee47a53ae5b2b1b00000000000000000000000000000051a5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff65701947831753dd7087cac61ab5644b308642cc1c33dc138f3cf7ad23cd3cadbd9735aff958023239c6a063';
+        // unpack bytes 
         (
             bytes32[2] memory properties,
             bytes16[1] memory minAmount,
             address[] memory middleTokens,
             bytes4[1] memory routerSelector
         ) = lighter.unpackBytes(clFunctionExampleBytesReturn);
+        // assert every data is unpacked correcty
         assertEq(properties[0], hex'0a73e24538d154555f286ce69a790402221b64536c2e4deceee47a53ae5b2b1b');
         assertEq(properties[1], hex'000000000000000000000000a5e0829caced8ffdd4de3c43696c57f7d7a678ff');
         assertEq(routerSelector[0], hex'65701947');
+        // console log length of middle path tokens
         console.log("Middle tokens to compose path: ", middleTokens.length);
-
+        // assert string min length
         vm.expectRevert("Data length must be >= 72!");
         lighter.unpackBytes(bytes("wrong length"));
 
         uint amountIn = 100000000000000;
-
+        // concatenate data via ConcatCalldata
         bytes memory concatenatedData = lighter.ConcatCalldata(amountIn, minAmount[0], middleTokens, routerSelector[0]);
         
-        bytes4 methodSig = 0x38ed1739;
-        bytes32 const = 0x00000000000000000000000000000000000000000000000000000000000000a0;
-        uint hops = 2;
+        bytes4 methodSig = 0x38ed1739; // lifi method signature for SwapGeneric
+        bytes32 const = 0x00000000000000000000000000000000000000000000000000000000000000a0; 
+        uint hops = 2; // hops in between in and out, provided in this example
         bytes memory path = abi.encodePacked(lighter.addressToBytes32(middleTokens[0]), lighter.addressToBytes32(middleTokens[1]), bytes28(0));
         assertEq(abi.encodePacked(methodSig, 
             bytes32(amountIn),
@@ -427,9 +468,13 @@ contract Test_Sample is Test {
             bytes32(hops), // this case has 2 mid tokens
             path
         ), concatenatedData);
+        
     }
 
     function test_readResponseAndSwap() public {
+        // This function is exected to revert, to test this out a new string must be provided
+        // from playground for example. Remove the expect revert and run test against mainnet via
+        // --fork-url <rpc-url>
         uint amountIn = 100000000000000;
         bytes memory clFunctionExampleBytesReturn = hex'0a73e24538d154555f286ce69a790402221b64536c2e4deceee47a53ae5b2b1b00000000000000000000000000000051a5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff65701947831753dd7087cac61ab5644b308642cc1c33dc138f3cf7ad23cd3cadbd9735aff958023239c6a063';
         vm.expectRevert(); // This will revert if not called via --fork-url || outdated data string
